008e5746f2e55a63c7621a59a48c32ab3d1e2a57
SonMooSans <god63820869@gmail.com>
2022-11-17 21:49:54 +0800

    Support specified guilds only commands

--- src/main/kotlin/net/sonmoosans/jdak/JDAK.kt
+++ src/main/kotlin/net/sonmoosans/jdak/JDAK.kt
@@ -1,11 +1,41 @@
 package net.sonmoosans.jdak
 
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.Guild
 import net.sonmoosans.jdak.builder.CommandBuilder
 import net.sonmoosans.jdak.listener.CommandListener
 
 object JDAK {
-    fun commands(jda: JDA, init: CommandBuilder.() -> Unit) {
+    fun guilds(jda: JDA, guilds: List<Guild>, init: CommandBuilder.() -> Unit) {
+        val builder = CommandBuilder().apply(init)
+        val listener = CommandListener()
+
+        for (command in builder.slashcommands) {
+            command.buildHandler(listener.slash)
+        }
+        for (command in builder.userCommands) {
+            listener.users[command.name] = command.handler
+        }
+        for (command in builder.messageCommands) {
+            listener.messages[command.name] = command.handler
+        }
+
+        val slashCommands = builder.slashcommands.map { it.build() }
+        val userCommands = builder.userCommands.map { it.build() }
+        val messageCommands = builder.messageCommands.map { it.build() }
+
+        for (guild in guilds) {
+            guild.updateCommands()
+                .addCommands(slashCommands)
+                .addCommands(userCommands)
+                .addCommands(messageCommands)
+                .queue()
+        }
+
+        jda.addEventListener(listener)
+    }
+
+    fun global(jda: JDA, init: CommandBuilder.() -> Unit) {
         val builder = CommandBuilder().apply(init)
         val listener = CommandListener()
 
@@ -24,6 +54,7 @@ object JDAK {
             .addCommands(builder.userCommands.map { it.build() })
             .addCommands(builder.messageCommands.map { it.build() })
             .queue()
+
         jda.addEventListener(listener)
     }
 }
