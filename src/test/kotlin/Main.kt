46415c709e3f56e567c2bbd8a23ff6d142d768bd
SonMooSans <god63820869@gmail.com>
2022-11-17 21:50:09 +0800

    Update Test

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.interactions.DiscordLocale
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
 import net.sonmoosans.jdak.JDAK
 import net.sonmoosans.jdak.builder.CommandBuilder
 import net.sonmoosans.jdak.builder.group
@@ -12,15 +14,32 @@ fun main() {
     val jda = JDABuilder.createDefault(System.getenv("TOKEN"))
         .build()
         .awaitReady()
+    val guilds = arrayOf("684766026776576052")
 
-    JDAK.commands(jda) {
-        usercontext("Test", mapOf(
-            DiscordLocale.CHINESE_TAIWAN to "測試"
-        )) {
+    JDAK.guilds(jda, guilds = guilds.map { jda.getGuildById(it)!! }) {
+        //inline
+        messagecommand("Test",
+            nameLocalizations = mapOf(DiscordLocale.CHINESE_TAIWAN to "測試"),
+            guildOnly = true,
+            permissions = DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)
+        ) {
             it.reply("Hello World").queue()
         }
 
+        //full
+        messagecontext("Test 2") {
+            nameLocalizations = mapOf(DiscordLocale.CHINESE_TAIWAN to "測試 2")
+            guildOnly = true
+            permissions = DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)
+
+            onEvent {
+                it.reply("Hello World").queue()
+            }
+        }
+
         slashcommand("test", "debug commands") {
+            guildOnly = true
+
             group("beta", "Beta features") {
                 subcommand("kill", "Kill someone") {
                     val target = user("target", "The target to kill")
