8849ac405b54e619713784b6f8a4414191738fd1
SonMooSans <god63820869@gmail.com>
2022-11-17 23:26:31 +0800

    Release to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.7.20"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    signing
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "io.github.sonmoosans"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -13,7 +16,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("net.dv8tion:JDA:5.0.0-alpha.22")
+    compileOnly("net.dv8tion:JDA:5.0.0-alpha.22")
+
+    testImplementation("net.dv8tion:JDA:5.0.0-alpha.22")
 }
 
 tasks.test {
@@ -22,4 +27,57 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("JDAK")
+                description.set("A Light-Weight, Fast, Flexible, Functional Programming Command framework for JDA written in Kotlin")
+                url.set("https://github.com/SonMooSans/jdak")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("http://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("SonMooSans")
+                        email.set("god63820869@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/SonMooSans/jdak.git")
+                    developerConnection.set("scm:git:ssh://github.com/SonMooSans/jdak.git")
+                    url.set("https://github.com/SonMooSans/jdak/tree/master")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(project.properties["username"]!!.toString())
+            password.set(project.properties["password"]!!.toString())
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
 }
